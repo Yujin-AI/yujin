f44862dd38b664420ccc7527b043321c59ed096a
Hitarashi <sayeed205@gmail.com>
2024-04-26 14:48:28 +0530

    add stream ai response

--- app/services/open_ai_service.ts
+++ app/services/open_ai_service.ts
@@ -1,8 +1,7 @@
-import OpenAI from 'openai'
-import { ChatCompletion, ChatCompletionChunk } from 'openai/resources/chat/completions'
-import { Stream } from 'openai/streaming'
-
 import { getToken } from '#lib/utils'
+import OpenAI from 'openai'
+import { ChatCompletionChunk } from 'openai/resources/chat/completions'
+import { Stream } from 'openai/streaming'
 
 export default class OpenAIService {
   private readonly openai: OpenAI
@@ -10,20 +9,35 @@ export default class OpenAIService {
     this.openai = new OpenAI({ apiKey: this.apiKey })
   }
 
-  async ask(context: OpenAI.Chat.Completions.ChatCompletionMessageParam[]): Promise<ChatCompletion>
-  async ask(
-    context: OpenAI.Chat.Completions.ChatCompletionMessageParam[],
-    stream = true
-  ): Promise<Stream<ChatCompletionChunk>> {
+  // async ask(context: OpenAI.Chat.Completions.ChatCompletionMessageParam[]): Promise<ChatCompletion>
+  async ask(context: OpenAI.Chat.Completions.ChatCompletionMessageParam[]): Promise<string> {
     const token = getToken(JSON.stringify(context)).length
     console.log('token:', token)
-    if (token > 16000) return
+    //if (token > 16000) return
 
     const response = await this.openai.chat.completions.create({
       messages: context,
       model: 'gpt-4-1106-preview',
       temperature: 0,
-      stream: stream ?? false,
     })
+
+    return response.choices[0].message.content ?? ''
+  }
+
+  async askStream(
+    context: OpenAI.Chat.Completions.ChatCompletionMessageParam[]
+  ): Promise<Stream<ChatCompletionChunk>> {
+    const token = getToken(JSON.stringify(context)).length
+    console.log('token:', token)
+    //if (token > 16000) return
+
+    const response = this.openai.chat.completions.create({
+      messages: context,
+      model: 'gpt-4-1106-preview',
+      temperature: 0,
+      stream: true,
+    })
+
+    return response
   }
 }
