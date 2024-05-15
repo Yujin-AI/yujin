d706e3426491dc6a48eff6947a86debda184c090
Hitarashi <sayeed205@gmail.com>
2024-05-15 10:21:18 +0530

    import type

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,4 +1,4 @@
-import { HttpContext } from "@adonisjs/core/http";
+import type { HttpContext } from "@adonisjs/core/http";
 
 import User from "#models/user";
 import { loginValidator, signUpValidator } from "#validators/auth_validator";
